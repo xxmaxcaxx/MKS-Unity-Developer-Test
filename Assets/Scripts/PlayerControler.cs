f0999a999d934aa966c3c081b5d31ecef6f26f85
FERNANDO HENRY HRUSCHKA <fernando_henryh@hotmail.com>
2022-10-11 15:50:27 -0300

    Add player movement

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -16,9 +16,17 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 offset = new Vector2 ( mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
 
-        transform.Translate(moveInput * Time.deltaTime * moveSpeed);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(-Vector2.up * Time.deltaTime * moveSpeed);
+        }
     }
 }
