2ec374c477707dac11e9c98341c3d43c3f369fa1
FERNANDO HENRY HRUSCHKA <fernando_henryh@hotmail.com>
2022-10-11 20:43:48 -0300

    Add game session timer

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
+    public TMP_Text textTimer;
+
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DisplayTime();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        DisplayTime();
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    void DisplayTime()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60.0f);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        textTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
